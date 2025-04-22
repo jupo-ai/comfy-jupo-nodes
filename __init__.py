7cc16039673eef050fb00ba312a46b8025ee464e
jupo-ai <jupojupo.ai@gmail.com>
2025-04-23 00:57:07 +0900

    add pixel_cleanup

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from .py.utils import _name, _dname
 from .py import save
 from .py import aspect_ratios
 from .py import sampler_selector
+from .py import pixel_cleanup
 
 NODE_CLASS_MAPPINGS = {
     _name("Save_Checkpoint"): save.SaveCheckpoint, 
     _name("Aspect_Ratios"): aspect_ratios.AspectRatios, 
     _name("Sampler_Selector"): sampler_selector.SamplerSelector, 
+    _name("Pixel_CleanUp"): pixel_cleanup.PixelCleanUp
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {k: _dname(k) for k in NODE_CLASS_MAPPINGS}
